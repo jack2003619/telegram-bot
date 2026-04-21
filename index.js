fde59a6585582694b9ede0b357de55a783870602
jack2003619 <nyi78317@gmail.com>
2026-04-22 01:20:42 +0630

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,74 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 
 app.use(express.json());
 
+// MongoDB Connect
+mongoose.connect(process.env.MONGO_URL)
+.then(() => console.log("MongoDB connected"))
+.catch(err => console.log(err));
+
+// User Schema
+const UserSchema = new mongoose.Schema({
+  username: String,
+  balance: { type: Number, default: 0 }
+});
+
+const User = mongoose.model("User", UserSchema);
+
+// Home
 app.get("/", (req, res) => {
   res.send("Zonguru Backend Running 🚀");
 });
 
-app.post("/deposit", (req, res) => {
-  const { user, amount } = req.body;
+// Create User
+app.post("/create-user", async (req, res) => {
+  const { username } = req.body;
+  const user = new User({ username });
+  await user.save();
+  res.json({ message: "User created", user });
+});
+
+// Get Users
+app.get("/users", async (req, res) => {
+  const users = await User.find();
+  res.json(users);
+});
+
+// Deposit
+app.post("/deposit", async (req, res) => {
+  const { username, amount } = req.body;
+
+  const user = await User.findOne({ username });
+  if (!user) return res.json({ message: "User not found" });
+
+  user.balance += Number(amount);
+  await user.save();
+
   res.json({
-    status: "success",
-    message: `User ${user} deposited ${amount} USDT`
+    message: `${username} deposited ${amount} USDT`,
+    balance: user.balance
   });
 });
 
-app.post("/withdraw", (req, res) => {
-  const { user, amount } = req.body;
+// Withdraw
+app.post("/withdraw", async (req, res) => {
+  const { username, amount } = req.body;
+
+  const user = await User.findOne({ username });
+  if (!user) return res.json({ message: "User not found" });
+
+  if (user.balance < amount) {
+    return res.json({ message: "Not enough balance" });
+  }
+
+  user.balance -= Number(amount);
+  await user.save();
+
   res.json({
-    status: "success",
-    message: `User ${user} withdraw ${amount} USDT`
+    message: `${username} withdraw ${amount} USDT`,
+    balance: user.balance
   });
 });
 
